d4cc1e25e77df12e6f2719d2458d068954334ee9
unknown <alhasankh58@gmail.com>
2023-04-08 14:58:31 +0300

    upadte my task

---
+++ task-4/1/script.js
@@ -0,0 +1,10 @@
+let nav = document.querySelector('nav');
+// let search =document.querySelector('search')
+let containersearch = document.querySelector('.container-search')
+
+nav.addEventListener('click', function(){
+    containersearch.classList.toggle('hide');
+    
+
+
+})
